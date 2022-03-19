f6e8d23ded6da4716c6a224168dde52eed4c623a
DESKTOP-0191A3N\Devin <DevinQi@outlook.com>
2022-03-19 00:07:43 -0400

    create private public key pairs, key hash, address and WIF private key

---
+++ keyAPI.py
@@ -0,0 +1,45 @@
+import ecdsa
+import ecdsa.der
+import ecdsa.util
+import hashlib
+import random
+import re
+import struct
+import base58check
+import binascii
+
+import utils
+
+
+def privateKeyToWif(private_key):
+    return utils.base58CheckEncode(hex(0x80), private_key)
+
+def wifToPrivateKey(wif):
+    return utils.base58CheckDecode(wif)
+
+def privateKeyToPublicKey(private_key):
+    sk = ecdsa.SigningKey.from_string(binascii.unhexlify(private_key), curve=ecdsa.SECP256k1)
+    hexsk = binascii.hexlify(sk.verifying_key.to_string())
+    return (b'04' + hexsk)
+
+def publicKeyToAddr(s):
+    ripemd160 = hashlib.new('ripemd160')
+    ripemd160.update(hashlib.sha256(s).digest())
+    return utils.base58CheckEncode(hex(0), binascii.hexlify(ripemd160.digest()).decode('utf-8'))
+
+def addrToScriptPubKey(b58str):
+    # 76     A9      14 (20 bytes)                                 88             AC
+    return '76a914' + utils.base58CheckDecode(b58str) + '88ac'
+
+
+#--------------------------------------------------------------------
+# Example
+# print(hex(a))
+private_key = 'ed17812c041ba14ba6cc6e3a1d6e6df50129643109dca5c63fb3ba12a95fcfe3'
+wif = privateKeyToWif(private_key)
+# ptest = wifToPrivateKey(wif)
+public_key = privateKeyToPublicKey(private_key)
+address = publicKeyToAddr(public_key)
+scriptPubKey = addrToScriptPubKey(address)
+print(scriptPubKey)
+
