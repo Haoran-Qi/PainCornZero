0b1658d4465cd7ff8fa9b48c00e19263fc966600
DESKTOP-0191A3N\Devin <DevinQi@outlook.com>
2022-03-21 12:07:26 -0400

    parse transaction to json

--- transactionAPI.py
+++ transactionAPI.py
@@ -1,3 +1,4 @@
+from cv2 import add
 import ecdsa
 import hashlib
 import struct
@@ -5,6 +6,26 @@ import struct
 import utils
 import keyAPI
 import binascii
+
+# parse transaction to json
+def parseJson(transaction):
+    parsed = parseTxn(transaction)
+    numOutputs = int(parsed[3][8:10], 16)
+    outputs = []
+    accIdx = 10
+    for i in range(numOutputs):
+        value = parsed[3][accIdx:accIdx + 16]
+        addressLen = int(parsed[3][accIdx+16:accIdx + 18], 16) * 2
+        address = parsed[3][accIdx + 18:accIdx +18+ addressLen ] 
+        outputs.append([value, address])
+        accIdx = accIdx +18+ addressLen
+    return{
+        "prevHash": parsed[0][10:74],
+        "sourceIdx":  parsed[0][74:],
+        "signature": parsed[1],
+        "publicKey": parsed[2],
+        "output": outputs
+    }
 
 # parse transaction information
 def parseTxn(txn):
@@ -74,30 +95,26 @@ def makeSignedTransaction(privateKey, outputTransactionHash, sourceIndex, script
 #--------------------------------------------------------------------
 # Example
 
-# # receiver
-# private_key = 'ed17812c041ba14ba6cc6e3a1d6e6df50129643109dca5c63fb3ba12a95fcfe3'
-# wif = keyAPI.privateKeyToWif(private_key)
-# public_key = keyAPI.privateKeyToPublicKey(private_key)
-# address = keyAPI.publicKeyToAddr(public_key)
-# scriptPubKey = keyAPI.addrToScriptPubKey(address)
+# receiver
+private_key = 'ed17812c041ba14ba6cc6e3a1d6e6df50129643109dca5c63fb3ba12a95fcfe3'
+wif = keyAPI.privateKeyToWif(private_key)
+public_key = keyAPI.privateKeyToPublicKey(private_key)
+address = keyAPI.publicKeyToAddr(public_key)
+scriptPubKey = keyAPI.addrToScriptPubKey(address)
 
-# # sender
-# prevHash = "81b4c832d70cb56ff957589752eb4125a4cab78a25a8fc52d6a09e5bd4404d48"
-# senderAddress = "1MMMMSUb1piy2ufrSguNUdFmAcvqrQF8M5"
-# senderWif = "5HusYj2b2x4nroApgfvaSfKYZhRbKFH41bVyPooymbC6KfgSXdD"
-# senderScriptPubKey = keyAPI.addrToScriptPubKey(senderAddress)
-# sprivatekey = keyAPI.wifToPrivateKey(senderWif)
-# spublick = keyAPI.privateKeyToPublicKey(sprivatekey)
+# sender
+prevHash = "81b4c832d70cb56ff957589752eb4125a4cab78a25a8fc52d6a09e5bd4404d48"
+senderAddress = "1MMMMSUb1piy2ufrSguNUdFmAcvqrQF8M5"
+senderWif = "5HusYj2b2x4nroApgfvaSfKYZhRbKFH41bVyPooymbC6KfgSXdD"
+senderScriptPubKey = keyAPI.addrToScriptPubKey(senderAddress)
+sprivatekey = keyAPI.wifToPrivateKey(senderWif)
+spublick = keyAPI.privateKeyToPublicKey(sprivatekey)
 
-# tpublic = '14e301b2328f17442c0b8310d787bf3d8a404cfbd0704f135b6ad4b2d3ee751310f981926e53a6e8c39bd7d3fefd576c543cce493cbac06388f2651d1aacbfcd'
-# tprivate = '0caecf01d74102a28aed6a64dcf1cf7b0e41c4dd6c62f70f46febdc32514f0bd'
+# make transaction
+txn = makeSignedTransaction(sprivatekey, prevHash, 0, keyAPI.addrToScriptPubKey(senderAddress), [[91234, keyAPI.addrToScriptPubKey(address)]])
 
-# txn = ("0100000001a97830933769fe33c6155286ffae34db44c6b8783a2d8ca52ebee6414d399ec300000000" +
-#         "8a47" +
-#         "304402202c2e1a746c556546f2c959e92f2d0bd2678274823cc55e11628284e4a13016f80220797e716835f9dbcddb752cd0115a970a022ea6f2d8edafff6e087f928e41baac01" +
-#         "41" +
-#         "04392b964e911955ed50e4e368a9476bc3f9dcc134280e15636430eb91145dab739f0d68b82cf33003379d885a0b212ac95e9cddfd2d391807934d25995468bc55" +
-#         "ffffffff02015f0000000000001976a914c8e90996c7c6080ee06284600c684ed904d14c5c88ac204e000000000000" +
-#         "1976a914348514b329fda7bd33c7b2336cf7cd1fc9544c0588ac00000000")
+# verrify transaction
+verifyTxnSignature(txn)
 
-# verifyTxnSignature(txn)
+# parse transaction
+parsed = parseJson(txn)
